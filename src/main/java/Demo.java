f82feaa9010d1d56654c0fa9bd9671069f2dee22
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-10-18 23:22:46 +0300

    functional interface

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,23 +1,30 @@
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
-import subscriber.SubscriberImpl;
 
+@Slf4j
 public class Demo {
 
     public static void main(String[] args){
-        Mono<String> mono = Mono.just("Mohamed");
+//        Mono<String> mono = Mono.just("Mohamed");
+//
+//        // Mono is one of implementation for publisher
+//        Publisher<String> mono2 = Mono.just("Mohamed");
+//
+//        /// It will print MonoJust (The type of variable go to section 1.1 in your red note)
+//        /// remeber that reactor like stream is lazy, as stream need to terminate method to work, reactor (publisher) need to be subscribed
+//        System.out.println(mono);
+//
+//        SubscriberImpl subscriber = new SubscriberImpl();
+//        mono.subscribe(subscriber);
+//
+//        /// Also it will not work we need request
+//        subscriber.getSubscription().request(5);
 
-        // Mono is one of implementation for publisher
-        Publisher<String> mono2 = Mono.just("Mohamed");
 
-        /// It will print MonoJust (The type of variable go to section 1.1 in your red note)
-        /// remeber that reactor like stream is lazy, as stream need to terminate method to work, reactor (publisher) need to be subscribed
-        System.out.println(mono);
+        /// Here you can not sever make at Publisher<Integer> as its subscribe method need full implementation for Subsriber interface which is not functional interface
+        Mono<Integer> mono = Mono.just(1);
 
-        SubscriberImpl subscriber = new SubscriberImpl();
-        mono.subscribe(subscriber);
-
-        /// Also it will not work we need request
-        subscriber.getSubscription().request(5);
+        mono.subscribe(i -> log.info("recieved: {}", i));
     }
 }
